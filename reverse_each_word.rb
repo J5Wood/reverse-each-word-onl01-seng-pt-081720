7f68addb24a2cc53be646bb69cfbfcacc092e581
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 23:10:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
 require 'pry'
 
 def reverse_each_word(sentence1)
-  split_list = sentence1.split(" ")
-  reversed_list = []
+  sentence1.split(" ").each{|x| x.reverse}
   split_list.each do |x|
-    reversed_list << x.reverse
-  end
-  reversed_list = reversed_list.join(" ")
-  reversed_list
+  end.join(" ")
 end
